61c4fd1cc77c455c61c4cfa4e622366474bb014d
usanihai <usaniha2@gmail.com>
2025-12-27 22:06:16 -0600

    add string file

---
+++ strings.py
@@ -0,0 +1,51 @@
+#print("Learning different typeof strings")
+
+#single quote string
+text = 'Hello Everyone'
+print(text)
+
+
+#double quote string
+goal = "Learn all automation skills "
+print(goal)
+
+# string with quotes inside
+whypython = "I heard, 'Python is easy for beginners'"
+print(whypython)
+
+#formatted string
+name = "Niha"
+age = 35
+print(f"My name is {name} and I am {age} years old")
+
+message = "Welcome to programming"
+print(message)
+
+path = r"C:\Users\Niha\Documents"
+print(path)
+
+#string concatenation
+
+first = "Good"
+second = "luck"
+third = "with Python"
+print(first + " " + second + " " + third)
+
+#multiline strings
+
+life = """We have only one life
+live life fully line 2
+Create memories and live stress free"""
+print(life)
+
+#string repeatation
+word = "love,'live,'life "
+print(word * 3)
+
+#accessig strings what will they do question for sandeep
+#text = "Python"
+#print(text[0])   # P
+#print(text[-1])  # n
+
+
+
