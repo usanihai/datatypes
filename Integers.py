82c117ffbb29244ade9467350d9fbac0f9e0a6fe
usanihai <usaniha2@gmail.com>
2025-12-27 22:23:38 -0600

    adding to new branch int

---
+++ Integers.py
@@ -0,0 +1,33 @@
+# simple integer and type
+alpha = 100
+print(alpha)
+print(type(alpha))
+
+#Negative Integer
+
+beta = -250
+print(beta)
+
+#zero as an integer
+null=0
+print(null)
+
+#large integer
+
+repeatednumber = 1234567801234567890
+print(repeatednumber)
+
+#Integer addition and substraction
+x = 5
+y = 3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+#converting data types to integer
+num_str = "100"
+num_float = 12.8
+
+print(int(num_str))    # 100
+print(int(num_float))  # 12
