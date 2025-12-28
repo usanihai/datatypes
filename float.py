5b1051a76de37549d210c544bbd1b9a02730b07d
usanihai <usaniha2@gmail.com>
2025-12-27 22:48:11 -0600

    added float to code

---
+++ float.py
@@ -0,0 +1,37 @@
+
+
+price = 19.99
+temperature = -3.5
+
+print(price)
+print(temperature)
+print(type(price))
+
+a = -8.25
+print(a)
+print(type(a))
+
+big_floatvalue = 123567.78903333
+print(big_floatvalue)
+
+small_floatvalue = 0.000022
+print(small_floatvalue)
+
+#float from integer
+num = float(19)
+print(num)
+#float from string
+
+#foat from string
+results = float("99.99")
+print(results)
+
+
+#float in calculations
+price = 9.99
+quantity = 5
+total = price * quantity
+
+print(total
+)
+
